065ef28ff48a41eeb10cf789ec2cca4a38ba3367
timoxa20 <95927253+timoxa20@users.noreply.github.com>
2024-04-19 22:45:37 +0300

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,11 +1,11 @@
 import HtmlWebpackPlugin from "html-webpack-plugin"
-import  webpack  from "webpack"
-import { BuildOptions } from "./types/config"
+import webpack from "webpack"
+import {BuildOptions} from "./types/config"
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer'
 
-export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[]  {
-    return [
+export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html
         }),
@@ -17,9 +17,15 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         new webpack.DefinePlugin({
             __IS_DEV_: JSON.stringify(isDev),
         }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false
-        }),
     ]
+    if (isDev) {
+        plugins.push(
+            new webpack.HotModuleReplacementPlugin(),
+        )
+        plugins.push(
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false
+            }),)
+    }
+    return plugins
 }
