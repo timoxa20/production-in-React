76210bb44c6d3f405cedbf24779e03a66b478d68
timoxa20 <95927253+timoxa20@users.noreply.github.com>
2024-04-20 15:30:21 +0300

    add themeDecorator

---
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -0,0 +1,13 @@
+import {Decorator} from '@storybook/react';
+import {Theme} from "app/providers/ThemeProvider/lib/ThemeContext";
+import {ThemeProvider} from "app/providers/ThemeProvider";
+import React from "react";
+
+// eslint-disable-next-line
+export const ThemeDecorator = (theme: Theme): Decorator => ( Story) => (
+    <ThemeProvider initialTheme={theme} >
+        <div className={`app ${theme}`}>
+            <Story/>
+        </div>
+    </ThemeProvider>
+)
