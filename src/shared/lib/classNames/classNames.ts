04c2e675d2e4a24be1b93c23be32fec624ffb590
timoxa20 <95927253+timoxa20@users.noreply.github.com>
2024-04-20 19:22:15 +0300

    fix preview

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -5,7 +5,8 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
         cls,
         ...additional.filter(Boolean),
         ...Object.entries(mods)
-            .filter(([ value]) => Boolean(value))
+            // eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+            .filter(([_, value]) => Boolean(value))
             .map(([className]) => className),
     ]
         .join(' ');
