0cc3774aba97ff998c2a74e5f4a07e55f9731868
timoxa20 <95927253+timoxa20@users.noreply.github.com>
2024-04-23 10:44:16 +0300

    add redux store, add first entity

--- src/pages/MainPages/ui/MainPage.tsx
+++ src/pages/MainPages/ui/MainPage.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import {useTranslation} from "react-i18next";
-import {Counter} from "../../../entities/Counter/ui/Counter";
 
 const MainPage = () => {
     const {t} = useTranslation('main')
     return (
         <div>
             { t('Главная страница') }
-            <Counter/>
         </div>
     );
 };
