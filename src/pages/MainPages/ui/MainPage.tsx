401d5d23363c881491a8a498a0c26cc84153e18e
timoxa20 <95927253+timoxa20@users.noreply.github.com>
2024-04-23 19:55:12 +0300

    add new input, add login, form ui

--- src/pages/MainPages/ui/MainPage.tsx
+++ src/pages/MainPages/ui/MainPage.tsx
@@ -1,20 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {useTranslation} from "react-i18next";
-import {Input} from "shared/ui/Input/Input";
 
 const MainPage = () => {
     const {t} = useTranslation('main')
-    const [value, setValue] = useState('')
-    const onChange = (val: string) => {
-        setValue(val)
-    }
+
     return (
         <div>
             {t('Главная страница')}
-            <Input
-                placeholder={'Введите текст'}
-                onChange={onChange}
-                value={value}/>
         </div>
     );
 };
