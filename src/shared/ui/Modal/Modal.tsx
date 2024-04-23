001bc050d82dd3cbc011fa7ee9e5e7e8d4528023
timoxa20 <95927253+timoxa20@users.noreply.github.com>
2024-04-23 12:53:08 +0300

    add theme for modal window

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -60,7 +60,7 @@ export const Modal = (props: ModalProps) => {
 
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className, theme])}>
+            <div className={classNames(cls.Modal, mods, [className, theme, " "])}>
                 <div className={cls.overlay} onClick={closeHandler}>
                     <div className={cls.content} onClick={onContentClick}>
                         {children}
