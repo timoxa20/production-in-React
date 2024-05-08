2278de13ba4629e9bb037dba29ffbd28876bbc52
timoxa20 <95927253+timoxa20@users.noreply.github.com>
2024-05-08 10:39:44 +0300

    add create, edit page route

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import {BuildOptions} from "./types/config"
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer'
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import CopyPlugin from "copy-webpack-plugin";
 
 export function buildPlugins({paths, isDev, apiUrl, project}: BuildOptions): webpack.WebpackPluginInstance[] {
     const plugins = [
@@ -19,6 +20,11 @@ export function buildPlugins({paths, isDev, apiUrl, project}: BuildOptions): web
             __IS_DEV_: JSON.stringify(isDev),
             __API__: JSON.stringify(apiUrl),
             __PROJECT__: JSON.stringify(project)
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: paths.locales, to: paths.buildLocales },
+            ],
         }),
     ]
     if (isDev) {
